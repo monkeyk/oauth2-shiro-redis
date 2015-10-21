30cc6ab5649de38759e924fe763690119d87e9fa
Li Shengzhao <shengzhan@uwantsoft.com>
2015-10-21 17:51:08 +0800

    Add CacheKeys, remove unused

---
+++ core/src/main/java/com/monkeyk/os/infrastructure/CacheKeys.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2015 MONKEYK Information Technology Co. Ltd
+ * www.monkeyk.com
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * MONKEYK Information Technology Co. Ltd ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use
+ * it only in accordance with the terms of the license agreement you
+ * entered into with MONKEYK Information Technology Co. Ltd.
+ */
+package com.monkeyk.os.infrastructure;
+
+/**
+ * 2015/10/21
+ *
+ * @author Shengzhao Li
+ */
+public abstract class CacheKeys {
+
+
+    /**
+     * AccessToken  cache
+     * key:  tokenId + cache_name
+     */
+    public static final String ACCESS_TOKEN_CACHE = "accessTokenCache";
+
+
+    //private
+    private CacheKeys() {
+    }
+
+}
